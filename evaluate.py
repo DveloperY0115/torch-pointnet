6c27d1ed615e9da052f3cc2d49573d7bc81c2e33
dveloperY0115 <dreamy1534@gmail.com>
2021-03-06 11:57:36 +0900

    Project Setup

---
+++ evaluate.py
@@ -0,0 +1,8 @@
+import torch
+import numpy as np
+import math
+import sys
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(BASE_DIR)
+sys.path.append(os.path.join(BASE_DIR, '../utils'))

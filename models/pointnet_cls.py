69460095184a20427edfcb88e6c58a40d137fc5a
Yoo SeungWoo <dreamy1534@gmail.com>
2021-03-08 17:08:11 +0900

    feat(main): Naive implementation of PointNet

--- models/pointnet_cls.py
+++ models/pointnet_cls.py
@@ -10,6 +10,18 @@ sys.path.append(BASE_DIR)
 sys.path.append(os.path.join('../utils'))
 from transform_nets import TransformNet
 
+# -------*-------
+# hack tensor_repr for easier debugging
+old_repr = torch.Tensor.__repr__
+
+
+def tensor_info(tensor):
+    return repr(tensor.shape)[6:] + ' ' + repr(tensor.dtype)[6:] + '@' + str(tensor.device) + '\n' + old_repr(tensor)
+
+
+torch.Tensor.__repr__ = tensor_info
+# -------*-------
+
 
 class PointNet(torch.nn.Module):
     """
@@ -21,27 +33,27 @@ class PointNet(torch.nn.Module):
         """
         super(PointNet, self).__init__()
 
-        # T-Net for input data
+        # T-Nets
         self.t_net_input = TransformNet(size=3)
         self.t_net_feature = TransformNet(size=64)
 
-        # MLPs for feature extraction
-        # Consists of 1-D convolution layers
-        self.mlp_64 = nn.Conv1d(in_channels=3, out_channels=64, kernel_size=1)
-        self.mlp_128 = nn.Conv1d(in_channels=64, out_channels=128, kernel_size=1)
-        self.mlp_1024 = nn.Conv1d(in_channels=128, out_channels=1024, kernel_size=1)
+        # MLPs -> Official implementation uses Conv Layers but following naive one suggested in the paper
+        self.mlp_3_64 = nn.Linear(in_features=3, out_features=64)
+        self.mlp_64_64_a = nn.Linear(in_features=64, out_features=64)
+        self.mlp_64_64_b = nn.Linear(in_features=64, out_features=64)
+        self.mlp_64_128 = nn.Linear(in_features=64, out_features=128)
+        self.mlp_128_1024 = nn.Linear(in_features=128, out_features=1024)
 
-        # Fully Connected layers for classification
-        self.fc1 = nn.Linear(1024, 512)
-        self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, num_classes)    # 9 Classes
+        # Fully Connected Layers
+        self.fc_1024_512 = nn.Linear(in_features=1024, out_features=512)
+        self.fc_512_256 = nn.Linear(in_features=512, out_features=256)
+        self.fc_256_k = nn.Linear(in_features=256, out_features=num_classes)
 
-        # Batch Normalization layers
-        self.bn1 = nn.BatchNorm1d(64)   # batch norm after mlp_64
-        self.bn2 = nn.BatchNorm1d(128)    # batch norm after mlp_128
-        self.bn3 = nn.BatchNorm1d(1024)    # batch norm after mlp_1024
-        self.bn4 = nn.BatchNorm1d(512)    # batch norm after fc1
-        self.bn5 = nn.BatchNorm1d(256)    # batch norm after fc2
+        # Batch normalization layers
+        self.bn_64_a = nn.BatchNorm1d(num_features=64)
+        self.bn_64_b = nn.BatchNorm1d(num_features=64)
+        self.bn_128 = nn.BatchNorm1d(num_features=128)
+        self.bn_1024 = nn.BatchNorm1d(num_features=1024)
 
     def forward(self, x):
         """
@@ -49,17 +61,45 @@ class PointNet(torch.nn.Module):
         :param x: Input data
         :return: Probability mass function containing probability of each class
         """
-        batch_size = x.shape[0]
-        x = self.t_net_input(x)
-        x = F.relu(self.bn1(self.mlp_64(x)))
-        x = self.t_net_feature(x)
-        x = F.relu(self.bn2(self.mlp_128(x)))
-        x = F.relu(self.bn3(self.mlp_1024(x)))
-        x = torch.max(x, 2, keepdim=True)[0]
-        x = x.view(-1, 1024)
+        batch_size = x.size()[0]
+        num_points = x.size()[1]
 
-        x = F.relu(self.bn4(self.fc1(x)))
-        x = F.relu(self.bn5(self.fc2(x)))
-        x = self.fc3(x)
+        x = self.t_net_input(x)    # (B, N, 3) -> (B, N, 3)
+        x = self.mlp_3_64(x)    # (B, N, 3) -> (B, N, 64)
+
+        # Batch normalization after 3 -> 64
+        x = x.view(batch_size, -1, num_points)
+        x = self.bn_64_a(x)
+        x = x.view(batch_size, num_points, -1)
+
+        x = self.mlp_64_64_a(x)    # (B, N, 64) -> (B, N, 64)
+
+        # Batch normalization after 64 -> 64
+        x = x.view(batch_size, -1, num_points)
+        x = self.bn_64_b(x)
+        x = x.view(batch_size, num_points, -1)
+
+        x = self.t_net_feature(x)    # (B, N, 64) -> (B, N, 64)
+
+        x = self.mlp_64_128(x)    # (B, N, 64) -> (B, N, 128)
+
+        # Batch normalization after 64 -> 128
+        x = x.view(batch_size, -1, num_points)
+        x = self.bn_128(x)
+        x = x.view(batch_size, num_points, -1)
+
+        x = self.mlp_128_1024(x)    # (B, N, 128) -> (B, N, 1024)
+
+        # Batch normalization after 128 -> 1024
+        x = x.view(batch_size, -1, num_points)
+        x = self.bn_1024(x)
+        x = x.view(batch_size, num_points, -1)
+
+        # Apply max pooling
+        x, _ = torch.max(x, dim=1)    # (B, N, 1024) -> (B, 1024)
+
+        x = self.fc_1024_512(x)
+        x = self.fc_512_256(x)
+        x = self.fc_256_k(x)
 
         return x
